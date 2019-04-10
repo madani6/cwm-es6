49ace16e4d32a156d55767fcb014d94ea4d43a46
madani6 <anandamadani06@gmail.com>
2019-04-10 09:27:49 +0700

    spread-operator

---
+++ spread-operator/app.js
@@ -0,0 +1,22 @@
+//   const first = [1, 2, 3];
+//   const second = [4, 5, 6];
+
+// //   const combined = first.concat(second);
+// //   const combined = [...first, 'a', ...second, 'b'];
+
+// const clone = [...first];
+// console.log(first);
+// console.log(clone);
+
+// const first = { name: 'Mosh'};
+// const second = { job: 'Instructor' };
+
+// const combined = {...first, ...second, location: 'Australia'};
+// console.log(combined);
+
+const first = { name: "Mosh" };
+const second = { job: "Instructor" };
+
+const combined = { ...first };
+console.log(second);
+console.log(combined);
