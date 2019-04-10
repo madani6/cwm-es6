6d85753b35ae9457515f454c3f7a262a72d89301
madani6 <anandamadani06@gmail.com>
2019-04-10 09:48:10 +0700

    inheritance

---
+++ inheritance/app.js
@@ -0,0 +1,22 @@
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  walk() {
+    console.log("walk");
+  }
+}
+
+class Teacher extends Person {
+  constructor(name, degree) {
+    super(name);
+    this.degree = degree;
+  }
+
+  teach() {
+    console.log("teach");
+  }
+}
+
+const teacher = new Teacher("Mosh", "MSc");
